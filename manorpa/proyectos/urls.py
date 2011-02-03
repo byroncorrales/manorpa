0ded9cb2aa519818a845c32f94424952917ca30b
Byron Corrales <bcorrales@bcorrales-laptop.(none)>
2011-02-03 10:37:10 -0600

    Agregando feeds para proyectos y documentos

--- manorpa/proyectos/urls.py
+++ manorpa/proyectos/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls.defaults import *
 from django.conf import settings
 from models import Proyecto
+from feeds import ProyectoFeed
 
 urlpatterns = patterns('proyectos.views',
+    (r'^proyectos/feed/$', ProyectoFeed()),
     (r'^proyectos/(?P<slug>[-\w]+)/$', 'proyecto_detalle'),
     (r'^proyectos/area/(?P<area_slug>[-\w]+)/$', 'proyecto_lista_area'),
     (r'^proyectos/$', 'proyecto_lista'),
